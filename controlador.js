84a1b2cefc63466f6eb1479fc1e5b4010eb33d79
jakelinepenagos1 <70981425+jakelinepenagos1@users.noreply.github.com>
2021-07-19 16:39:33 -0500

    repaso

---
+++ controlador.js
@@ -0,0 +1,34 @@
+
+
+function sumarNumeros(numero1,numero2){
+
+    let suma=numero1+numero2;
+    console.log("la suma es:"+suma)
+
+}
+sumarNumeros(5,10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    if(true){
+
+    }
+    
+    for(let i=0; i<100; i++){
+
+    }
